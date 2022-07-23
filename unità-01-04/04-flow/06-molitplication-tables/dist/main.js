29781fc9deba1bcccd97b21b2cd30e452ec99d83
KelosDev <gpiraswebdev@gmail.com>
2022-07-23 15:34:05 +0200

    agg

---
+++ unità-01-04/04-flow/06-molitplication-tables/dist/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let tables = (num) => {
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${num} * ${i} = ${num * i}`);
+    }
+};
+tables(2);
+tables(7);
---
+++ unità-01-04/04-flow/06-molitplication-tables/src/main.ts
@@ -0,0 +1,10 @@
+//tabelline
+
+let tables = (num:number) => {
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${num} * ${i} = ${num*i}`);
+    }
+}
+
+tables(2);
+tables(7);
